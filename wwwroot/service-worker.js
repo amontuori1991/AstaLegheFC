b14f5e86686fe59d160f2ccfb07d451b019758ce
amontuori1991 <alexmontuori1991@gmail.com>
2025-08-05 16:22:24 +0200

    App smartphone

---
+++ wwwroot/service-worker.js
@@ -0,0 +1,13 @@
+﻿// service-worker.js
+
+// Questo service worker è intenzionalmente lasciato vuoto.
+// La sua presenza è sufficiente per rendere l'app installabile (PWA).
+// In futuro, potrà essere usato per gestire la cache e le funzionalità offline.
+
+self.addEventListener('install', (event) => {
+    // console.log('Service Worker: Installazione...');
+});
+
+self.addEventListener('fetch', (event) => {
+    // Al momento non intercettiamo le richieste
+});
