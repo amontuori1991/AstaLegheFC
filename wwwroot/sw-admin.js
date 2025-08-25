49345def7e14772db5baa82a5e3a87381bd0da47
amontuori1991 <alexmontuori1991@gmail.com>
2025-08-25 16:33:35 +0200

    Fix log

---
+++ wwwroot/sw-admin.js
@@ -0,0 +1,9 @@
+﻿self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(clients.claim()); });
+
+self.addEventListener('fetch', (event) => {
+    if (event.request.mode === 'navigate') {
+        event.respondWith(fetch(event.request));
+        return;
+    }
+});
---
+++ wwwroot/sw-utente.js
@@ -0,0 +1,11 @@
+﻿self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(clients.claim()); });
+
+// Navigazioni: prendi sempre dalla rete (niente app-shell che “sporca” l’Utente)
+self.addEventListener('fetch', (event) => {
+    if (event.request.mode === 'navigate') {
+        event.respondWith(fetch(event.request));
+        return;
+    }
+    // qui opzionale: cache statici per /Utente/ (css/js/img)
+});
